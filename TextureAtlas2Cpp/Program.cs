7f56892065720fc0502e88f8fc3f9e8766b33ae8
El_lo <El_lo@DESKTOP-MHCUQET>
2021-08-16 13:55:00 +0200

    Agregar archivos de proyecto.

---
+++ TextureAtlas2Cpp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Xml;
+
+namespace TextureAtlas2Cpp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source, headerFile;
+            XmlDocument doc;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguments passed");
+
+                return;
+            }
+
+            source = args[0];
+
+            doc = HeaderGenerator.ValidateFile(source, out headerFile);
+
+            if (doc == null)
+            {
+                return;
+            }
+
+            if (HeaderGenerator.GenerateHeader(doc, headerFile))
+            {
+                Console.WriteLine("Success!");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
